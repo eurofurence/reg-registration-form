3d868488f8edbf2d610af50bebc17030524b7ea1
Fenmar <xfenmar@gmail.com>
2019-06-24 22:25:50 +0200

    don't use input fields on the submit page

--- submit.js
+++ submit.js
@@ -8,16 +8,15 @@ document.addEventListener("DOMContentLoaded", async () => {
     lang = config.defaultLanguage;
   }
 
+  restoreForm();
+
   setupCountries(config.availableCountries, lang);
   setupTShirts(config.tShirt, lang);
   setupPartner(config.partnerVisible);
-  setupOptions("flags", config.flags, lang);
-  setupOptions("options", config.options, lang);
-  setupPackages(config.tiers, config.packages, lang);
-
-  restoreForm();
-
-  hideUnchecked();
+  setupOptions(config.flags, config.options, lang);
+  setupPackages(config.packages, lang);
+  setupBirthday();
+  setupGender();
 
   setupSubmitButton(config.regStart, config.apiEndpoint);
 });
@@ -27,34 +26,21 @@ function setupCountries(countries, lang) {
     '[data-field="country"],[data-field="country_badge"]'
   );
 
-  const options = [];
-  Object.keys(countries).forEach(key => {
-    options.push({ key, value: countries[key][lang] });
-  });
-  options.sort((a, b) => {
-    return a.value > b.value ? 1 : -1;
-  });
-
   for (let i = 0; i < elements.length; i++) {
-    options.forEach(({ key, value }) => {
-      const option = document.createElement("option");
-      option.setAttribute("value", key);
-      option.textContent = value;
-
-      elements[i].appendChild(option);
-    });
+    elements[i].textContent = countries[elements[i].textContent][lang];
   }
+}
+
+function setupBirthday() {
+  const element = document.querySelector('[data-field="birthday"]');
+  element.textContent = new Date(element.textContent).toLocaleDateString();
 }
 
 function setupTShirts(tshirts, lang) {
   const element = document.querySelector('[data-field="tshirt_size"]');
-  tshirts.forEach(el => {
-    const option = document.createElement("option");
-    option.setAttribute("value", el.code);
-    option.textContent = el[lang];
-
-    element.appendChild(option);
-  });
+  element.textContent = tshirts.find(
+    ({ code }) => code === element.textContent
+  )[lang];
 }
 
 function setupPartner(visible) {
@@ -64,45 +50,53 @@ function setupPartner(visible) {
   }
 }
 
-function setupOptions(selector, options, lang) {
-  const container = document.getElementById(selector);
-  options.forEach(option => {
-    const entry = document.createElement("div");
-    entry.innerHTML = `<label>
-    <input disabled type="checkbox" data-field="${selector}:${
-      option.code
-    }"></input>
-      <span>${option[lang].label}</span>
-    </label>
-    `;
+function setupOptions(flags, options, lang) {
+  let data = localStorage.getItem("regFormData");
+  if (data) {
+    data = JSON.parse(data);
+    const container = document.getElementById("options");
 
-    container.appendChild(entry);
-  });
+    flags.forEach(flag => {
+      if (data["flags:" + flag.code]) {
+        container.innerHTML += `<span>${flag[lang].label}</span>`;
+      }
+    });
+    options.forEach(option => {
+      if (data["options:" + option.code]) {
+        container.innerHTML += `<span>${option[lang].label}</span>`;
+      }
+    });
+  }
 }
 
-function setupPackages(tiers, packages, lang) {
-  const container = document.getElementById("packages");
-  const table = document.createElement("table");
+function setupGender() {
+  let data = localStorage.getItem("regFormData");
+  if (data) {
+    data = JSON.parse(data);
+    const gender = data.gender;
 
-  // body
-  const body = document.createElement("tbody");
-  body.innerHTML = packages
-    .map(
-      package => `<tr>
-    <td>
-      <label>
-      <input disabled type="checkbox" data-field="packages:${
-        package.code
-      }"></input>
-      <span>${package[lang].label}</span>
-      </label>
-    </td>
-  </tr>`
-    )
-    .join("");
-  table.appendChild(body);
+    const entries = document.querySelectorAll(".gender");
 
-  container.appendChild(table);
+    for (let i = 0; i < entries.length; i++) {
+      if (entries[i].getAttribute("data-key") !== gender) {
+        entries[i].parentNode.removeChild(entries[i]);
+      }
+    }
+  }
+}
+
+function setupPackages(packages, lang) {
+  let data = localStorage.getItem("regFormData");
+  if (data) {
+    data = JSON.parse(data);
+    const container = document.getElementById("packages");
+
+    packages.forEach(package => {
+      if (data["packages:" + package.code]) {
+        container.innerHTML += `<span>${package[lang].label}</span>`;
+      }
+    });
+  }
 }
 
 function restoreForm() {
@@ -115,21 +109,10 @@ function restoreForm() {
         if (typeof value === "boolean") {
           document.querySelector(`[data-field="${key}"]`).checked = value;
         } else {
-          document.querySelector(`[data-field="${key}"]`).value = value;
+          document.querySelector(`[data-field="${key}"]`).textContent = value;
         }
       }
     });
-  }
-}
-
-function hideUnchecked() {
-  const elements = document.querySelectorAll('fieldset input[type="checkbox"]');
-
-  for (let i = 0; i < elements.length; i++) {
-    if (!elements[i].checked) {
-      const container = elements[i].parentNode.parentNode;
-      container.parentNode.removeChild(container);
-    }
   }
 }
 
